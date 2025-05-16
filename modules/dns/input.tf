c887bcf48d90458ed3e92a534caf1d9dcb783619
HRathore2411 <harsh.rathore@nenosystems.in>
2025-05-16 18:53:02 +0530

    add new folder

---
+++ modules/dns/input.tf
@@ -0,0 +1,13 @@
+variable "USE_MONGODB" {
+  default = ""
+}
+
+variable "USE_RDS" {
+  type    = bool
+  default = false
+}
+
+variable "test34" {
+  type    = bool
+  default = false
+}
---
+++ templates/cloud-manager/output.tf
@@ -0,0 +1,11 @@
+output "bastion_network_interface" {
+  value = aws_network_interface.nuxeo.id
+}
+
+output "test" {
+  value = ""
+}
+
+output "test1" {
+  value = ""
+}
