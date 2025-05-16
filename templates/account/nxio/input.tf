b6ae2b7b84f2834d0cfc563372103bb1d7c28c9f
HRathore2411 <harsh.rathore@nenosystems.in>
2025-05-16 15:41:12 +0530

    add input.tf in nxio

---
+++ templates/account/nxio/input.tf
@@ -0,0 +1,15 @@
+# tflint-ignore: terraform_unused_declarations
+variable "NUXEO_IMAGE_NAME" {
+  type    = string
+  default = ""
+}
+
+variable "test" {
+  type    = bool
+  default = false
+}
+
+variable "test1" {
+  type    = bool
+  default = false
+}
